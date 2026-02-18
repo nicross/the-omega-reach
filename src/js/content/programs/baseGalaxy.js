7bca870b2eb6962e87a96f605426f8f333e03918
nicross <nicross@users.noreply.github.com>
2026-02-17 22:30:51 -0600

    Improve irregular galaxy geometry

--- src/js/content/programs/baseGalaxy.js
+++ src/js/content/programs/baseGalaxy.js
@@ -6,7 +6,7 @@ content.programs.baseGalaxy = content.programs.invent({
   propertyDefinitions: {
     // Particles
     galaxyRadius: function () {return engine.fn.lerp(3, 6, this.options.galaxy.mass)},
-    irregularRadiusScale: (srand) => srand(0.5, 2.5),
+    irregularRadiusScale: (srand) => srand(1, 3),
     rotation: (srand) => engine.tool.quaternion.fromEuler({
       pitch: srand(-Math.PI, Math.PI),
       roll: srand(-Math.PI, Math.PI),
